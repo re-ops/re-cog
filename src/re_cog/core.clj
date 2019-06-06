63c18ad5e644575896d850a3cbd14429abaa62ea
narkisr <narkisr@gmail.com>
2019-06-06 22:58:59 +1000

    Moving fn-meta to cog

--- src/re_cog/core.clj
+++ src/re_cog/core.clj
@@ -1,1 +1,21 @@
-(ns re-cog.core)
+(ns re-cog.core
+  (:require
+   re-cog.resources.exec
+   re-cog.resources.file
+   re-cog.resources.package
+   re-cog.facts.oshi
+   re-cog.facts.query
+   re-cog.facts.security))
+
+(def functions
+  (apply merge
+         (map ns-map
+              ['re-cog.resources.exec 're-cog.resources.file 're-cog.resources.package
+               're-cog.facts.oshi 're-cog.facts.query 're-cog.facts.security])))
+
+(defn fn-meta [f]
+  {:post [#(not (nil? %))]}
+  (meta
+   (second
+    (first
+     (filter #(and (var? (second %)) (= f (var-get (second %)))) functions)))))
