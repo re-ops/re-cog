8fe39ee1f7325b858211e6477581eaaa4a771094
narkisr <narkisr@gmail.com>
2019-06-10 00:03:56 +1000

    adding package

--- src/re_cog/recipes/osquery.clj
+++ src/re_cog/recipes/osquery.clj
@@ -1,11 +1,14 @@
 (ns re-cog.recipes.osquery
   (:require
-   [re-cog.common :refer (require-functions def-inline)]
-   [re-cog.resources.download :refer (download)]))
+   [re-cog.common :refer (require-functions require-constants def-inline)]
+   [re-cog.resources.download :refer (download)]
+   [re-cog.resources.package :refer (package)]))
 
 (require-functions)
+(require-constants)
 
 (def-inline install
   "Installing osquery"
   [archive sum]
-  (download (<< "https://pkg.osquery.io/deb/~{archive}") (<< "/tmp/~{archive}") sum))
+  (download (<< "https://pkg.osquery.io/deb/~{archive}") (<< "/tmp/~{archive}") sum)
+  (package (<< "/tmp/~{archive}") :present))
