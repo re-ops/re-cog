33403306bbf680b6e90a01b550a313e9814bbbb9
ronen <narkisr@gmail.com>
2019-06-29 23:32:53 +1000

    extracting checksum into a separate function

--- src/re_cog/resources/download.clj
+++ src/re_cog/resources/download.clj
@@ -1,16 +1,21 @@
 (ns re-cog.resources.download
   (:require
-   [re-cog.common :refer (require-functions def-serial)]))
+   [re-cog.common.functions :refer (require-functions)]
+   [re-cog.common :refer (def-serial)]))
 
 (require-functions)
+
+(def-serial checksum
+  "File checksum with optional hash-type (:sha256 by default)"
+  [f sum & opts]
+  (assert (= sum (file-checksum f opts))))
 
 (def-serial download
   "Download file resource, if checksum is provided download will be lazy:
       (download url dest expected :sha256) ; download only if file missing or checksum mismatch
       (download url dest); always download "
-  [url dest checksum & {:keys [hash-type] :or {hash-type :sha256}}]
-  (let [digest-fns {:sha256 digest/sha-256 :md5 digest/md5}]
-    (when (or (not (exists? dest)) (not (= checksum ((digest-fns hash-type) (io/as-file dest)))))
-      (with-open [in (io/input-stream url) out (io/output-stream dest)]
-        (io/copy in out))
-      (assert (= checksum ((digest-fns hash-type) (io/as-file dest)))))))
+  [url dest sum & opts]
+  (when (or (not (exists? dest)) (not (= sum (file-checksum dest opts))))
+    (with-open [in (io/input-stream url) out (io/output-stream dest)]
+      (io/copy in out))
+    (assert (= sum (file-checksum dest opts)))))
