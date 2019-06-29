caf255332d84a76831f18de3f8cd21c7e706a9d3
ronen <narkisr@gmail.com>
2019-06-29 23:32:14 +1000

    using let form instead of args

--- src/re_cog/recipes/osquery.clj
+++ src/re_cog/recipes/osquery.clj
@@ -1,6 +1,7 @@
 (ns re-cog.recipes.osquery
   (:require
-   [re-cog.common :refer (require-functions require-constants def-inline)]
+   [re-cog.common.functions :refer (require-functions)]
+   [re-cog.common :refer (require-constants def-inline)]
    [re-cog.resources.download :refer (download)]
    [re-cog.resources.permissions :refer (set-file-acl)]
    [re-cog.resources.file :refer (copy)]
@@ -11,8 +12,10 @@
 
 (def-inline install
   "Installing osquery"
-  [archive sum]
-  (download (<< "https://pkg.osquery.io/deb/~{archive}") (<< "/tmp/~{archive}") sum)
-  (package (<< "/tmp/~{archive}") :present)
-  (set-file-acl "re-ops" "rwX" "/etc/osquery")
-  (copy "/tmp/resources/osquery.conf" "/etc/osquery/osquery.conf"))
+  []
+  (let [archive "osquery_3.3.2_1.linux.amd64.deb"
+        sum "6c54a55df1feaf410064bc87696f9829d0787fb5b9c7beabeb4f5cca7ce6c3fb"]
+    (download (<< "https://pkg.osquery.io/deb/~{archive}") (<< "/tmp/~{archive}") sum)
+    (package (<< "/tmp/~{archive}") :present)
+    (set-file-acl "re-ops" "rwX" "/etc/osquery")
+    (copy "/tmp/resources/osquery.conf" "/etc/osquery/osquery.conf")))
