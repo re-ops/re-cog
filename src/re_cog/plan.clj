c8c6ff91f95e41c9e4cfdffc24349a646a5ffd60
ronen <narkisr@gmail.com>
2019-09-04 00:32:16 +1000

    even nicer type separation

--- src/re_cog/plan.clj
+++ src/re_cog/plan.clj
@@ -5,13 +5,15 @@
    [loom.graph :as g]))
 
 (def base-setup
-  ['re-cog.recipes.build 're-cog.recipes.nvim 're-cog.recipes.shell])
+  ['re-cog.recipes.shell])
 
 (def osquery (into ['re-cog.recipes.osquery] base-setup))
 
 (def virtual (into ['re-cog.recipes.virtualization] base-setup))
 
 (def backup (into ['re-cog.recipes.backup] base-setup))
+
+(def dev (into ['re-cog.recipes.clojure 're-cog.recipes.build 're-cog.recipes.nvim] base-setup))
 
 (defn all-functions [namespaces]
   (mapcat
