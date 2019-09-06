af2355185581c0d2a8308cdb0100bf6fb8d9dcc3
ronen <narkisr@gmail.com>
2019-09-06 23:40:13 +1000

    Adding tooling ns

---
+++ src/re_cog/recipes/tooling.clj
@@ -0,0 +1,20 @@
+(ns re-cog.recipes.tooling
+  "Common tools"
+  (:require
+   [re-cog.resources.download :refer (download)]
+   [re-cog.resources.file :refer (rename symlink chmod)]
+   [re-cog.resources.archive :refer (untar bzip2)]
+   [re-cog.common.functions :refer (require-functions require-resources)]
+   [re-cog.common :refer (require-constants)]
+   [re-cog.resources.permissions :refer (set-file-acl)]
+   [re-cog.common.defs :refer (def-inline)]))
+
+(def-inline disk-tools
+  "Common disk utilities"
+  (package "gt5" :present))
+
+(def-inline security-tools
+  "Common security tooling"
+  (package "pwgen" :present)
+  (package "veracrypt" :present))
+
