664623b2e8c8c8a4213239dcb850eb5cd4ebe1d3
ronen <narkisr@gmail.com>
2019-09-27 17:40:49 +1000

    bumping share and making a release

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject re-cog "0.2.0"
+(defproject re-cog "0.2.1"
   :description "Distributed provisioning Clojure functions to be used with Re-gent"
   :url "https://github.com/re-ops/re-cog"
   :license {:name "Apache License, Version 2.0" :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [
-     [org.clojure/clojure "1.10.0"]
+     [org.clojure/clojure "1.10.1"]
      [org.clojure/core.incubator "0.1.4"]
 
      ; serialization
@@ -26,7 +26,7 @@
      [com.fzakaria/slf4j-timbre "0.3.8"]
 
      ; common utilities and shared functions
-     [re-share "0.11.8"]
+     [re-share "0.11.9"]
      [re-scan "0.2.1"]
 
      ; clojure to bash
