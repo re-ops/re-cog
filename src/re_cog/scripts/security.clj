8cf72aba411a5abd18a6f8099310900842053159
ronen <narkisr@gmail.com>
2019-11-23 00:07:46 +1100

    ssh-connections script

--- src/re_cog/scripts/security.clj
+++ src/re_cog/scripts/security.clj
@@ -8,4 +8,14 @@
    (set! R @("sudo" "ufw" "status"))
    (if (not (= $? 0)) ("exit" 1))
    (when (= @R "Status: inactive") ("echo" "firewall disabled!") ("exit" 1))
-   (pipe (pipe (pipe ((println (quoted "${R}"))) ("awk" "'NR>4'")) ("sed -r 's/  +/,/g'")) ("tr" "-s" "' '"))))
+   (pipe
+    (pipe
+     (pipe ((println (quoted "${R}"))) ("awk" "'NR>4'"))
+     ("sed -r 's/  +/,/g'"))
+    ("tr" "-s" "' '"))))
+
+(defn ssh-connections []
+  (script
+   (pipe
+    (pipe ("sudo" "netstat" "-tnpa") ("grep" "'ESTABLISHED.*sshd'"))
+    ("tr" "-s" "' '"))))
