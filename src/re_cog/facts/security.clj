3a5aa0dabf227d61fa1516b24b23e22860800766
ronen <narkisr@gmail.com>
2019-12-31 18:41:52 +1100

    Adding cpu vulns

--- src/re_cog/facts/security.clj
+++ src/re_cog/facts/security.clj
@@ -1,6 +1,8 @@
 (ns re-cog.facts.security
   "security related functions"
   (:require
+   [me.raynes.fs :as fs]
+   [clojure.java.io :as io]
    [re-scan.core :refer [into-ports into-hosts nmap]]
    [re-cog.common.defs :refer (def-serial)]))
 
@@ -13,3 +15,10 @@
   "Host addresses nmap scan"
   [path flags network]
   (into-hosts (nmap path flags network)))
+
+(def-serial cpu-vulns
+  "Grab a list of cpu vulns for a host"
+  []
+  (into {}
+        (map (fn [f] [(keyword (.getName f)) (slurp f)])
+             (filter fs/file? (file-seq (io/file "/sys/devices/system/cpu/vulnerabilities/"))))))
