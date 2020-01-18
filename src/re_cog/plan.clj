b6a29c98117df72bfa8889b53d887f0a181ff08c
ronen <narkisr@gmail.com>
2020-01-18 21:49:22 +1100

    re organizing plans

--- src/re_cog/plan.clj
+++ src/re_cog/plan.clj
@@ -4,54 +4,69 @@
    [loom.alg :as alg]
    [loom.graph :as g]))
 
+; Basic profiles
 (def ^{:doc "Minimal set of machine addones"}
-  lean ['re-cog.recipes.shell 're-cog.recipes.hardening])
+  lean #{'re-cog.recipes.shell 're-cog.recipes.hardening})
 
 (def ^{:doc "Base setup common to all plans (shell, hardening, osquery etc.)"}
-  base (into ['re-cog.recipes.osquery 're-cog.recipes.monitoring] lean))
+  base (into #{'re-cog.recipes.osquery 're-cog.recipes.monitoring} lean))
+
+; Infra profiles
+(def ^{:doc "re-core ready instances"}
+  core #{'re-cog.recipes.clojure 're-cog.recipes.build 're-cog.recipes.nvim 're-cog.recipes.shell 're-cog.recipes.hardening})
+
+(def nas (into #{'re-cog.recipes.backup 're-cog.recipes.zfs} base))
+
+(def wireguard #{'re-cog.recipes.hardening 're-cog.recipes.wireguard})
+
+; Container/Virtualization 
 
 (def ^{:doc "Virtualization tools (KVM, LXC)"}
-  virtual (into ['re-cog.recipes.virtualization] base))
-
-(def ^{:doc "Docker server"}
-  docker (into ['re-cog.recipes.docker] base))
-
-(def ^{:doc "Backup tools"}
-  backup (into ['re-cog.recipes.backup] base))
-
-(def ^{:doc "Cloud tools (gcloud, doctl, awscli)"}
-  cloud (into ['re-cog.recipes.cloud] base))
-
-(def nas (into ['re-cog.recipes.backup 're-cog.recipes.zfs] base))
-
-(def wireguard ['re-cog.recipes.hardening 're-cog.recipes.wireguard])
-
-(def ^{:doc "An instance with just nvim"}
-  editing ['re-cog.recipes.nvim])
-
-(def ^{:doc "Development machine with Clojure and Graal"}
-  native-clj (into ['re-cog.recipes.clojure 're-cog.recipes.build 're-cog.recipes.nvim 're-cog.recipes.graal] base))
-
-(def ^{:doc "Development machine with Clojure and Graal"}
-  dev (into ['re-cog.recipes.clojure 're-cog.recipes.build 're-cog.recipes.nvim 're-cog.recipes.python] base))
-
-(def ^{:doc "Development machine with Clojure and deep learning utils"}
-  learning (into ['re-cog.recipes.clojure 're-cog.recipes.build 're-cog.recipes.nvim 're-cog.recipes.deep] base))
-
-(def ^{:doc "A Vuepress documentation instance"}
-  vuepress (into ['re-cog.recipes.node 're-cog.recipes.nvim] lean))
-
-(def ^{:doc "re-core ready instances"}
-  core ['re-cog.recipes.clojure 're-cog.recipes.build 're-cog.recipes.nvim 're-cog.recipes.shell 're-cog.recipes.hardening])
+  virtual (into #{'re-cog.recipes.virtualization} base))
 
 (def ^{:doc "minikube"}
-  minikube ['re-cog.recipes.k8s 're-cog.recipes.docker])
+  minikube #{'re-cog.recipes.k8s 're-cog.recipes.docker})
 
-(def ^{:doc "security"}
-  security ['re-cog.recipes.security])
+(def ^{:doc "Docker server"}
+  docker (into #{'re-cog.recipes.docker} base))
 
-(def ^{:doc "iot development machine"}
-  iot-dev (into ['re-cog.recipes.platformio] dev))
+(def ^{:doc "Backup tools"}
+  backup (into #{'re-cog.recipes.backup} base))
+
+(def ^{:doc "Cloud tools (gcloud, doctl, awscli)"}
+  cloud (into #{'re-cog.recipes.cloud} base))
+
+(def ^{:doc "An instance with just nvim"}
+  editing #{'re-cog.recipes.nvim})
+
+(def ^{:doc "Security utilities"}
+  security #{'re-cog.recipes.security})
+
+; Development profiles
+
+(def ^{:doc "Base dev support"}
+  base-dev (into #{'re-cog.recipes.build 're-cog.recipes.nvim} base))
+
+(def ^{:doc "Clojure development instance"}
+  clj-dev (into #{'re-cog.recipes.clojure} base-dev))
+
+(def ^{:doc "Development machine with Clojure and Graal"}
+  native-clj (into #{'re-cog.recipes.graal} clj-dev))
+
+(def ^{:doc "Python development machine"}
+  python-dev (into #{'re-cog.recipes.nvim 're-cog.recipes.python} base))
+
+(def ^{:doc "Support for Java/Kotlin and Clojure development"}
+  jvm-dev (into #{'re-cog.recipes.intellij} clj-dev))
+
+(def ^{:doc "Development machine with Clojure and deep learning utils"}
+  learning (into #{'re-cog.recipes.clojure 're-cog.recipes.build 're-cog.recipes.nvim 're-cog.recipes.deep} base))
+
+(def ^{:doc "A Vuepress documentation instance"}
+  vuepress (into #{'re-cog.recipes.node 're-cog.recipes.nvim} lean))
+
+(def ^{:doc "IoT development instance"}
+  iot-dev (into #{'re-cog.recipes.platformio} base-dev))
 
 (defn all-functions [namespaces]
   (mapcat
