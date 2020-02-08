acce67da94991601af97b45dc272ff4750fc32e9
ronen <narkisr@gmail.com>
2020-02-08 21:00:50 +1100

    fixing version

--- bin/binary.sh
+++ bin/binary.sh
@@ -1,2 +1,2 @@
 LEIN_SNAPSHOTS_IN_RELEASE=1 lein with-profile package do uberjar
-cat bin/stub.sh target/re-cog-0.2.4-standalone.jar > target/re-cog && chmod +x target/re-cog
+cat bin/stub.sh target/re-cog-0.3.1-standalone.jar > target/re-cog && chmod +x target/re-cog
