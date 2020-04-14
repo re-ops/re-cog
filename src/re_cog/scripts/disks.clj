918869e49076bb6e418374b94fbec3c71136c2ed
ronen <narkisr@gmail.com>
2020-04-14 22:26:34 +1000

    Adding fn in order to support args in the script

--- src/re_cog/scripts/disks.clj
+++ src/re_cog/scripts/disks.clj
@@ -5,8 +5,9 @@
 (defn single-mkfs-ext4
   "Create a single partition of ext4 on a device and mount"
   [device target]
-  (script
-   ("sudo" "/usr/sbin/parted" ~device "--script" "--" "mklabel" "msdos")
-   ("sudo" "/usr/sbin/parted" "-s" "-a" "optimal" "--" ~device "mkpart" "primary" "0" "-1")
-   ("sudo" "/usr/sbin/mkfs.ext4" ~device "-F")
-   ("sudo" "/usr/bin/mount" ~device ~target)))
+  (fn []
+    (script
+     ("sudo" "/usr/sbin/parted" ~device "--script" "--" "mklabel" "msdos")
+     ("sudo" "/usr/sbin/parted" "-s" "-a" "optimal" "--" ~device "mkpart" "primary" "0" "-1")
+     ("sudo" "/usr/sbin/mkfs.ext4" ~device "-F")
+     ("sudo" "/usr/bin/mount" ~device ~target))))
