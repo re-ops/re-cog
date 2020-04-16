82ce0c0df77bb34cb11293b1bf384bfb83c87fe7
ronen <narkisr@gmail.com>
2020-04-16 21:59:20 +1000

    Seperating mount and partitioning

--- src/re_cog/scripts/disks.clj
+++ src/re_cog/scripts/disks.clj
@@ -2,12 +2,16 @@
   "Scripts for disk manipulation"
   (:require  [pallet.stevedore :refer (script)]))
 
-(defn single-mkfs-ext4
+(defn partition-
   "Create a single partition of ext4 on a device and mount"
-  [device target]
+  [device]
   (fn []
     (script
      ("sudo" "/usr/sbin/parted" ~device "--script" "--" "mklabel" "msdos")
      ("sudo" "/usr/sbin/parted" "-s" "-a" "optimal" "--" ~device "mkpart" "primary" "0" "-1")
-     ("sudo" "/usr/sbin/mkfs.ext4" ~device "-F")
+     ("sudo" "/usr/sbin/mkfs.ext4" ~device "-F"))))
+
+(defn mount [device target]
+  (fn []
+    (script
      ("sudo" "/usr/bin/mount" ~device ~target))))
