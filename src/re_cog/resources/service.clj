fda4d539f94c8900d192522478ad53403df094bd
ronen <narkisr@gmail.com>
2020-06-27 23:12:46 +1000

    Fixing service on-boot and moving to run-

--- src/re_cog/resources/service.clj
+++ src/re_cog/resources/service.clj
@@ -1,7 +1,7 @@
 (ns re-cog.resources.service
   (:require
-   [re-cog.common.functions :refer (require-functions)]
    [re-cog.common.defs :refer (def-serial)]
+   [re-cog.common.functions :refer (require-functions)]
    [re-cog.common.constants :refer (require-constants)]))
 
 (require-functions)
@@ -10,15 +10,19 @@
 (def-serial service
   "Service start/stop/restart"
   [srv state]
-  (do
+  (letfn [(service- [srv state]
+            (fn []
+              (script
+               ("sudo" ~systemd-bin ~srv ~state))))]
     (assert (#{:start :stop :restart} state))
-    (sh "sudo" systemd-bin srv (name state))))
+    (run- (service- srv (name state)))))
 
 (def-serial on-boot
   "Manage on boot service state enable/disable"
   [srv state]
-  (do (assert (#{:enable :disable} state))
-      (let [{:keys [out] :as m} (sh systemctl-bin "list-units" "--full" "-all")]
-        (if (.contains out srv)
-          (sh "sudo" systemctl-bin (name state) srv)
-          m))))
+  (letfn [(set-boot [srv stats]
+            (fn []
+              (script
+               ("sudo" ~systemctl-bin ~state ~srv))))]
+    (assert (#{:enable :disable} state))
+    (run- (set-boot srv (name state)))))
