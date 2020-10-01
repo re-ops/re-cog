1aff2f2cb29983d30bcd49e2d9a9f12e821ff8cd
ronen <narkisr@gmail.com>
2020-10-02 01:32:30 +1000

    Adding letsencrypt

---
+++ src/re_cog/scripts/letsencrypt.clj
@@ -0,0 +1,14 @@
+(ns re-cog.scripts.letsencrypt
+  (:require
+   [pallet.stevedore :refer (script)]
+   [clojure.core.strint :refer (<<)]))
+
+(defn update-certs
+  "Update certificates using dns-01 challenge"
+  [user token]
+  (fn []
+    (script
+     ("export" (set! PROVIDER "cloudflare"))
+     ("export" (set! LEXICON_CLOUDFLARE_USERNAME user))
+     ("export" (set! LEXICON_CLOUDFLARE_TOKEN token))
+     ("/srv/dehydrated/dehydrated" "--cron" "--hook" "/srv/dehydrated/dehydrated.default.sh" "--challenge" "dns-01"))))
