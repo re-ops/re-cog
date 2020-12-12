40d9ad1514dedda49b263156602916516e5a28a4
ronen <narkisr@gmail.com>
2020-12-12 15:29:03 +1100

    Adding shallow clone support

--- src/re_cog/resources/git.clj
+++ src/re_cog/resources/git.clj
@@ -34,7 +34,7 @@
     (failure "repository is missing")))
 
 (def-serial pull
-  "Pull implementation"
+  "Pull changes from upstream repository"
   [repo dest]
   (let [git (binary)
         {:keys [exit] :as binary-install} (install-missing git)]
@@ -45,15 +45,32 @@
           (run- (fn [] (script (~git ~dir "pull")))))
         (failure (<< "Skipping pull remote ~{repo} is no found under ~{dest}"))))))
 
-(def-serial clone
-  "Clone implementation"
-  [repo dest]
+(def-serial checkout
+  "Checkout a git branch"
+  [repo dest branch]
   (let [git (binary)
         {:keys [exit] :as binary-install} (install-missing git)]
     (if-not (= 0 exit)
       binary-install
+      (if (= 0 (:exit (repo-exists? repo dest)))
+        (let [dir (<< "--git-dir=~{dest}.git")]
+          (run- (fn [] (script (~git ~dir "checkout" "-b" ~branch)))))
+        (failure (<< "Skipping pull remote ~{repo} is no found under ~{dest}"))))))
+
+(def-serial clone
+  "Clone implementation"
+  [repo dest opts]
+  (let [git (binary)
+        {:keys [exit] :as binary-install} (install-missing git)
+        {:keys [depth]} opts]
+    (if-not (= 0 exit)
+      binary-install
       (letfn [(clone-script []
-                (script (~git "clone" ~repo ~dest)))]
+                (script (~git "clone" ~repo ~dest)))
+              (shallow-clone []
+                             (script (~git "clone" ~repo ~dest "--depth" ~depth)))]
         (if-not (= 0 (:exit (repo-exists? repo dest)))
-          (run- clone-script)
-          (success (<< "Skipping clone ~{repo} exists under ~{dest}")))))))
+          (cond
+            (not (nil? depth)) (run- shallow-clone)
+            :else (run- clone-script))
+          (success (<< "Skipping clone ~{repo} already exists under ~{dest}")))))))
