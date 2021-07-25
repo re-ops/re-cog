4899537e297cc7364b5ad487174e57bbbbbd9e15
ronen <narkisr@gmail.com>
2021-07-25 22:03:36 +1000

    adding more args to script

--- src/re_cog/scripts/nebula.clj
+++ src/re_cog/scripts/nebula.clj
@@ -3,7 +3,9 @@
    [pallet.stevedore :refer (script)]
    [clojure.core.strint :refer (<<)]))
 
-(defn sign [name ip groups]
+(defn sign [name ip groups crt key dest]
   (fn []
-    (script
-     ("/opt/nebula/nebula-cert" "sign" "-name" ~name "-ip" ~ip "-groups" ~groups))))
+    (let [out-key (str dest "/" name ".key")
+          out-crt (str dest "/" name ".crt")]
+      (script
+       ("/opt/nebula/nebula-cert" "sign" "-name" ~name "-ip" ~ip "-groups" ~groups "-ca-crt" ~crt "-ca-key" ~key "-out-key" ~out-key "-out-crt" ~out-crt)))))
