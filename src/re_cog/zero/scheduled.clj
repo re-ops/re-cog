58fcb82794aa4bb617c66a7135d7696fcec967ba
ronen <narkisr@gmail.com>
2021-08-02 19:09:30 +1000

    making missing result to match re-mote spec

--- src/re_cog/zero/scheduled.clj
+++ src/re_cog/zero/scheduled.clj
@@ -1,12 +1,14 @@
 (ns re-cog.zero.scheduled
   "Scheduled results handling"
   (:require
+   [clojure.core.strint :refer (<<)]
    [re-cog.common.defs :refer (def-serial)]))
 
 (def scheduled-results (atom {}))
 
 (defn get-result [k]
-  ((deref scheduled-results) k))
+  (get (deref scheduled-results) k
+       {:exit 1 :out (<< "missing result for ~{k} in scheduled results") :err ""}))
 
 (def-serial get-scheduled-result
   "Get a result for a scheduled function"
