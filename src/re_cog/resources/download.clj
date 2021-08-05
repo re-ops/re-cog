e7e864e48acfa34053f2c54798929dbcf0a08e82
ronen <narkisr@gmail.com>
2021-08-06 00:58:24 +1000

    better error message when parent folder is missing

--- src/re_cog/resources/download.clj
+++ src/re_cog/resources/download.clj
@@ -16,12 +16,14 @@
       (download url dest expected :hash-type :sha256) ; download only if file missing or checksum mismatch
      "
   [url dest sum & opts]
-  (if (or (not (fs/exists? dest)) (not (= sum (file-checksum dest opts))))
-    (do
-      (with-open [in (io/input-stream url) out (io/output-stream dest)]
-        (io/copy in out))
-      (let [found (file-checksum dest opts)]
-        (if (= sum found)
-          (success "Downloaded file successfuly")
-          (failure (<< "Found checksum ~{found} of downloaded file does not match provided ~{sum}")))))
-    (success "File already download and checksum matches")))
+  (if (not (fs/exists? (fs/parent dest)))
+    (failure (<< "parent folder missing, cannot download file into ~{dest}"))
+    (if (or (not (fs/exists? dest)) (not (= sum (file-checksum dest opts))))
+      (do
+        (with-open [in (io/input-stream url) out (io/output-stream dest)]
+          (io/copy in out))
+        (let [found (file-checksum dest opts)]
+          (if (= sum found)
+            (success "Downloaded file successfuly")
+            (failure (<< "Found checksum ~{found} of downloaded file does not match provided ~{sum}")))))
+      (success "File already download and checksum matches"))))
